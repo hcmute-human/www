e45f60f4a90914cd103452ded5e8c412a72a4a4e
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-09-24 23:13:47 +0700

    feat: configure api-client in startup

---
+++ app/startup.server.ts
@@ -0,0 +1,14 @@
+import { ApiClient } from '@lib/services/api-client.server';
+
+export function startup() {
+	if (!process.env.API_BASE_URL) {
+		throw new ReferenceError(
+			'API_BASE_URL environment variable must be provided'
+		);
+	}
+
+	ApiClient.use({
+		baseUrl: process.env.API_BASE_URL ?? '',
+		version: 'v1',
+	});
+}
