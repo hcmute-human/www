f2d3678db278fccf82ccc2f2f7378450321e9f94
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-09-25 10:01:12 +0700

    chore: format code

--- app/lib/components/Label.tsx
+++ app/lib/components/Label.tsx
@@ -1,12 +1,12 @@
-import { cn } from "@lib/utils";
-import { type LabelProps, Label as AriaLabel } from "react-aria-components";
+import { cn } from '@lib/utils';
+import { type LabelProps, Label as AriaLabel } from 'react-aria-components';
 
-const baseClass = "text-sm text-neutral-700";
+const baseClass = 'text-sm text-neutral-700';
 
 export default function Label({ className, children, ...props }: LabelProps) {
-  return (
-    <AriaLabel {...props} className={cn(baseClass, className)}>
-      {children}
-    </AriaLabel>
-  );
+	return (
+		<AriaLabel {...props} className={cn(baseClass, className)}>
+			{children}
+		</AriaLabel>
+	);
 }
--- app/lib/components/TextField.tsx
+++ app/lib/components/TextField.tsx
@@ -1,59 +1,59 @@
-import { cn } from "@lib/utils";
-import { forwardRef } from "react";
-import Input from "./Input";
-import Label from "./Label";
+import { cn } from '@lib/utils';
+import { forwardRef } from 'react';
+import Input from './Input';
+import Label from './Label';
 import {
-  type TextFieldProps,
-  Text,
-  TextField as AriaTextField,
-} from "react-aria-components";
+	type TextFieldProps,
+	Text,
+	TextField as AriaTextField,
+} from 'react-aria-components';
 
 interface Props extends TextFieldProps {
-  name: string;
-  label?: string;
-  description?: string;
-  errorMessage?: string;
-  labelClassName?: string;
-  inputClassName?: string;
+	name: string;
+	label?: string;
+	description?: string;
+	errorMessage?: string;
+	labelClassName?: string;
+	inputClassName?: string;
 }
 
 const TextField = forwardRef<HTMLInputElement, Props>(function TextField(
-  {
-    label,
-    labelClassName,
-    inputClassName,
-    description,
-    errorMessage,
-    ...props
-  }: Props,
-  ref
+	{
+		label,
+		labelClassName,
+		inputClassName,
+		description,
+		errorMessage,
+		...props
+	}: Props,
+	ref
 ) {
-  const invalid = errorMessage != null;
-  return (
-    <AriaTextField ref={ref} {...props} isInvalid={invalid}>
-      <Label className={cn("mb-0.5", labelClassName)}>{label}</Label>
-      <Input
-        ref={ref}
-        name={props.name}
-        className={cn(inputClassName, { "peer border-negative-500": invalid })}
-      />
-      {description ? (
-        <Text slot="description" className="text-neutral-700 text-xs mt-0.5">
-          {description}
-        </Text>
-      ) : null}
-      {invalid ? (
-        <Text
-          slot="errorMessage"
-          className="text-negative-500 text-xs mt-0.5
+	const invalid = errorMessage != null;
+	return (
+		<AriaTextField ref={ref} {...props} isInvalid={invalid}>
+			<Label className={cn('mb-0.5', labelClassName)}>{label}</Label>
+			<Input
+				ref={ref}
+				name={props.name}
+				className={cn(inputClassName, { 'peer border-negative-500': invalid })}
+			/>
+			{description ? (
+				<Text slot="description" className="text-sm text-neutral-700 mt-0.5">
+					{description}
+				</Text>
+			) : null}
+			{invalid ? (
+				<Text
+					slot="errorMessage"
+					className="text-sm text-negative-500 mt-0.5
 					peer-rac-invalid:transition peer-rac-invalid:ease-in-out peer-rac-invalid:duration-300
 					peer-rac-invalid:animate-in peer-rac-invalid:fade-in peer-rac-invalid:slide-in-from-top-4"
-        >
-          {errorMessage}
-        </Text>
-      ) : null}
-    </AriaTextField>
-  );
+				>
+					{errorMessage}
+				</Text>
+			) : null}
+		</AriaTextField>
+	);
 });
 
 export default TextField;
