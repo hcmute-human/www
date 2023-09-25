d26f40bb43f985215d13ae4993e5392be9a61bc5
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-09-25 10:02:59 +0700

    feat(components): add Form component

---
+++ app/lib/components/Form.tsx
@@ -0,0 +1,43 @@
+import { zodResolver } from '@hookform/resolvers/zod';
+import { FormProps, Form as RemixForm, useActionData } from '@remix-run/react';
+import { useEffect } from 'react';
+import {
+	FieldValues,
+	FormProvider,
+	Path,
+	UseFormProps,
+	useForm,
+} from 'react-hook-form';
+
+type Props<T extends FieldValues> = FormProps & {
+	options?: Omit<UseFormProps<T>, 'resolver'> &
+		Partial<{ schema: Zod.ZodType }>;
+};
+
+export default function Form<T extends FieldValues>({
+	options,
+	...props
+}: Props<T>) {
+	const methods = useForm<T>({
+		...options,
+		resolver: options?.schema ? zodResolver(options.schema) : undefined,
+	});
+
+	const data = useActionData<{ errors?: Record<string, string> }>();
+	const errors = data?.errors as Record<string, string> | undefined;
+
+	useEffect(() => {
+		if (!errors) {
+			return;
+		}
+
+		for (const [k, v] of Object.entries(errors)) {
+			methods.setError(k as Path<T>, { message: v });
+		}
+	}, [errors, methods]);
+	return (
+		<FormProvider {...methods}>
+			<RemixForm {...props} />
+		</FormProvider>
+	);
+}
