1075e0905e4f05da00830cbde963e7ab46d00432
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-09-29 01:40:19 +0700

    types: add Either type

--- app/lib/@types/index.d.ts
+++ app/lib/@types/index.d.ts
@@ -1,4 +1,7 @@
 declare type ActionError = Record<string, string>;
+declare type Either<T1, T2> =
+  | (T1 & { [key in keyof T2]?: never })
+  | (T2 & { [key in keyof T1]?: never });
 
 declare namespace NodeJS {
   interface ProcessEnv {
