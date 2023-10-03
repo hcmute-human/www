314a00148cad08fa4ffb563046369bcdd22bb288
NNQA <anhla12h@gmail.com>
2023-10-03 21:57:52 +0700

    fix: added a navlink prop for the Link component

--- app/lib/components/Link.tsx
+++ app/lib/components/Link.tsx
@@ -1,28 +1,33 @@
 import { cn } from '@lib/utils';
-import type { HTMLAttributes, ReactNode } from 'react';
+import type { AnchorHTMLAttributes, HTMLAttributes, ReactNode } from 'react';
 import { Link as AriaLink } from 'react-aria-components';
-import { Link as RemixLink, type LinkProps } from '@remix-run/react';
+import {
+  Link as RemixLink,
+  type LinkProps,
+  type NavLinkProps,
+  NavLink,
+} from '@remix-run/react';
 
 type Props =
   | ({ as?: 'link' } & LinkProps)
-  | ({ as: 'a' } & HTMLAttributes<HTMLAnchorElement>);
+  | ({ as: 'a' } & AnchorHTMLAttributes<HTMLAnchorElement>)
+  | ({ as?: 'navlink' } & NavLinkProps);
 
 const baseClass = 'text-accent-500 underline underline-offset-2';
 
-export default function Link({
-  className,
-  as = 'link',
-  children,
-  ...props
-}: Props) {
+export default function Link({ className, as = 'link', ...props }: Props) {
   let node: ReactNode | null = null;
   switch (as) {
     case 'link': {
-      node = <RemixLink {...(props as LinkProps)}>{children}</RemixLink>;
+      node = <RemixLink {...(props as LinkProps)} />;
       break;
     }
     case 'a': {
-      node = <a {...props}>{children}</a>;
+      node = <a {...(props as AnchorHTMLAttributes<HTMLAnchorElement>)} />;
+      break;
+    }
+    case 'navlink': {
+      node = <NavLink {...(props as NavLinkProps)} />;
       break;
     }
   }
