28ca3e5cc63ced3f79411f3cf1aa789232a881c8
NNQA <anhla12h@gmail.com>
2023-10-05 03:12:32 +0700

    feat(home): added ToolBar

--- app/routes/_app._index/LayoutHeader.tsx
+++ app/routes/_app._index/LayoutHeader.tsx
@@ -3,14 +3,17 @@ import NavigationBar, {
   type PropsItemNavgigate,
 } from '~/routes/_app._index/NavigationBar';
 import {
+  ArrowLeftOnRectangleIcon,
+  BellIcon,
+  Cog6ToothIcon,
   MapIcon,
   RectangleGroupIcon,
   Square2StackIcon,
   WrenchIcon,
 } from '@heroicons/react/20/solid';
-import { OverlayArrow, Tooltip, TooltipTrigger } from 'react-aria-components';
+import ToolBar, { type PropsItemToolbar } from './ToolBar';
 
-const items: Array<PropsItemNavgigate> = [
+const itemsnavigate: Array<PropsItemNavgigate> = [
   {
     text: 'Home',
     href: '/',
@@ -32,12 +35,33 @@ const items: Array<PropsItemNavgigate> = [
     Icon: <MapIcon className="h-5 w-5"></MapIcon>,
   },
 ];
+const itemstoolbar: Array<PropsItemToolbar> = [
+  {
+    text: 'Notification',
+    href: '/notification',
+    Icon: <BellIcon className="h-5 w-5"></BellIcon>,
+  },
+  {
+    text: 'Setting',
+    href: '/setting',
+    Icon: <Cog6ToothIcon className="h-5 w-5"></Cog6ToothIcon>,
+  },
+  {
+    text: 'Logout',
+    Icon: (
+      <ArrowLeftOnRectangleIcon className="h-5 w-5"></ArrowLeftOnRectangleIcon>
+    ),
+  },
+];
 function LayoutHeader() {
   return (
-    <header className="lg:h-screen lg:max-w-[13rem] lg:w-full flex lg:flex-col border-solid lg:border-r-[2px] border-b-[2px]">
-      <div className="flex lg:flex-col items-center gap-6 w-full">
-        <Logo className="h-6"></Logo>
-        <NavigationBar items={items}></NavigationBar>
+    <header className="lg:h-screen lg:max-w-[13rem] lg:w-full flex justify-between lg:flex-col border-solid lg:border-r-[2px] border-b-[2px]">
+      <div className="flex lg:flex-col items-center gap-3 w-full">
+        <Logo className="h-6 mt-7"></Logo>
+        <NavigationBar items={itemsnavigate}></NavigationBar>
+      </div>
+      <div className="">
+        <ToolBar items={itemstoolbar}></ToolBar>
       </div>
     </header>
   );
