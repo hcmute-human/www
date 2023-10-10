b007704ec0e2a5ed61310a9527f7b07fbc6db3fd
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-10 09:59:11 +0700

    feat: add _auth layout transition

--- app/routes/_auth/route.tsx
+++ app/routes/_auth/route.tsx
@@ -1,12 +1,31 @@
+import { AnimatedOutlet } from '@components/AnimatedOutlet';
 import MeshGradient from '@components/MeshGradient';
-import { Outlet } from '@remix-run/react';
+import { useLocation } from '@remix-run/react';
+import clsx from 'clsx';
+import { SwitchTransition } from 'transition-hook';
 
 export default function Route() {
+  const location = useLocation();
   return (
     <>
       <MeshGradient className="fixed w-screen h-screen opacity-5 bg-accent-500" />
       <div className="relative min-h-screen min-w-screen flex justify-center py-[20vh]">
-        <Outlet />
+        <SwitchTransition state={location.pathname} timeout={100} mode="out-in">
+          {(_, stage) => (
+            <div
+              className={clsx(
+                'transition-opacity duration-100',
+                {
+                  from: 'opacity-0 ease-in',
+                  enter: '',
+                  leave: 'opacity-0 ease-out',
+                }[stage]
+              )}
+            >
+              <AnimatedOutlet />
+            </div>
+          )}
+        </SwitchTransition>
       </div>
     </>
   );
