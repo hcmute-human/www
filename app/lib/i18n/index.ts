349a11ca13266ebfdba43cff7b4ab6bd4af65707
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-13 00:04:53 +0700

    fix(i18n): add missing namespacess

--- app/lib/i18n/index.ts
+++ app/lib/i18n/index.ts
@@ -2,7 +2,7 @@ export const config = {
   supportedLngs: ['en'],
   fallbackLng: 'en',
   defaultNS: 'common',
-  ns: ['common', 'login'],
+  ns: ['common', 'login', 'reset-password', 'reset-password-$token'],
   react: { useSuspense: false },
   keySeparator: '.',
 };
