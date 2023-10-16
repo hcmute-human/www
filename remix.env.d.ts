878901099c44f9b4c1057295263bdefa6f150497
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-16 15:29:46 +0700

    types: declare RouteHandle; augment useMatches

--- remix.env.d.ts
+++ remix.env.d.ts
@@ -2,11 +2,18 @@
 /// <reference types="@remix-run/node" />
 
 import type { Session } from '@remix-run/node';
+import type { UIMatch as UIMatch } from '@remix-run/react';
+import type { ReactNode } from 'react';
 
 declare global {
   interface SessionData {
     accessToken: string;
     refreshToken: string;
+  }
+
+  interface RouteHandle {
+    i18n?: string | string[];
+    breadcrumb?: (match?: UIMatch<unknown, RouteHandle>) => ReactNode;
   }
 }
 
@@ -14,4 +21,8 @@ declare module '@remix-run/server-runtime' {
   export interface AppLoadContext {
     session: Session<SessionData, unknown>;
   }
+}
+
+declare module '@remix-run/react' {
+  export function useMatches(): UIMatch<unknown, RouteHandle>[];
 }
