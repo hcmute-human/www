ec8e5d491b4f20a89a1baf1a1949966fa23c1071
NNQA <anhla12h@gmail.com>
2023-10-19 23:32:59 +0700

    feat(i18n): add config home.json for i18n

--- app/lib/i18n/index.ts
+++ app/lib/i18n/index.ts
@@ -2,7 +2,7 @@ export const config = {
   supportedLngs: ['en'],
   fallbackLng: 'en',
   defaultNS: 'common',
-  ns: ['common', 'login', 'reset-password', 'reset-password-$token'],
+  ns: ['common', 'login', 'reset-password', 'reset-password-$token, home'],
   react: { useSuspense: false },
   keySeparator: '.',
 };
