2fa115c6bf2122cf555c3ae2166ee8221e90627e
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-20 09:13:04 +0700

    feat: add homepage meta

--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -1,17 +1,30 @@
 import { Outlet } from 'react-router-dom';
 import LayoutBar from './LayoutBar';
 import ToolBar from './ToolBar';
-
+import type { LoaderFunctionArgs, MetaFunction } from '@remix-run/node';
+import i18next from '@lib/i18n/index.server';
 
 export function handle() {
-  return { i18n: 'home' };
+  return { i18n: ['meta', 'home'] };
 }
+
+export const meta: MetaFunction<typeof loader> = ({ data: { title } = {} }) => {
+  return [{ title }];
+};
+
+export async function loader({ request }: LoaderFunctionArgs) {
+  const title = await i18next
+    .getFixedT(request, 'meta')
+    .then((x) => x('home.title'));
+  return { title };
+}
+
 function App() {
   return (
     <div className="flex flex-col lg:flex-row w-full min-h-screen">
       <LayoutBar />
       <div className="bg-primary-0 flex-grow">
-        <div className='lg:inline-block w-full hidden'>
+        <div className="lg:inline-block w-full hidden">
           <ToolBar />
         </div>
         <Outlet />
