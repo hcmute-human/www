db037f72c9d99403c14d9325993207585d86c21e
NNQA <anhla12h@gmail.com>
2023-10-21 16:40:41 +0700

    feat: add hanlde for home

--- app/routes/_app/route.tsx
+++ app/routes/_app/route.tsx
@@ -2,24 +2,40 @@ import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import ToolBar from './ToolBar';
 import { useMatches } from '@remix-run/react';
+import Breadscumb from '@components/Breadcrumb';
+import Link from '@components/Link';
+
+export const handle = {
+  breadcrumb: () => <Link href="/">Home</Link>,
+};
 
 function App() {
   const matches = useMatches();
-
   return (
     <div className="flex flex-col lg:flex-row w-full min-h-screen">
       <Header />
       <div className="bg-primary-0 flex-grow px-4 py-2">
         <div className="flex items-center justify-between">
-          <h1>
-            {(matches.at(-1)?.data as Record<string, string | undefined> | null)
-              ?.title ?? ''}
-          </h1>
+          <div className="">
+            <Breadscumb items={matches}></Breadscumb>
+          </div>
           <div className="hidden lg:block">
             <ToolBar />
           </div>
         </div>
-        <Outlet />
+        <div className="h-[80vh]">
+          <div className="items-center">
+            <h1>
+              {(
+                matches.at(-1)?.data as unknown as Record<
+                  string,
+                  string | undefined
+                > | null
+              )?.title ?? ''}
+            </h1>
+            <Outlet />
+          </div>
+        </div>
       </div>
     </div>
   );
