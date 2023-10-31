eb7c7101757d35283a004ddb3811ce7f061ccc26
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-31 17:40:15 +0700

    feat: add translation strings for departments

--- app/lib/i18n/index.ts
+++ app/lib/i18n/index.ts
@@ -2,7 +2,14 @@ export const config = {
   supportedLngs: ['en'],
   fallbackLng: 'en',
   defaultNS: 'common',
-  ns: ['common', 'login', 'reset-password', 'reset-password-$token, home'],
+  ns: [
+    'common',
+    'login',
+    'reset-password',
+    'reset-password-$token',
+    'home',
+    'departments',
+  ],
   react: { useSuspense: false },
   keySeparator: '.',
 };
