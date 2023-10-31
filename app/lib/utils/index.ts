505443d85f76948529556e51be42783b81d1bfa7
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-31 17:14:18 +0700

    feat(utils): add filter, sort, searchParams utils

--- app/lib/utils/index.ts
+++ app/lib/utils/index.ts
@@ -1,5 +1,11 @@
 import type { Submission } from '@conform-to/react';
 import { parse } from '@conform-to/zod';
+import { compareItems, rankItem } from '@tanstack/match-sorter-utils';
+import {
+  sortingFns,
+  type FilterFn,
+  type SortingFn,
+} from '@tanstack/react-table';
 import { clsx, type ClassValue } from 'clsx';
 import { useId } from 'react';
 import { twMerge } from 'tailwind-merge';
@@ -61,3 +67,28 @@ export function parseSubmission<Schema extends ZodTypeAny>(
     ok: submission.intent === 'submit' && !!submission.value,
   } as SubmissionWithOk<output<Schema>>;
 }
+
+export const fuzzyFilter: FilterFn<unknown> = (
+  row,
+  columnId,
+  value,
+  addMeta
+) => {
+  const itemRank = rankItem(row.getValue(columnId), value);
+  addMeta({
+    itemRank,
+  });
+  return itemRank.passed;
+};
+
+export const fuzzySort: SortingFn<any> = (rowA, rowB, columnId) => {
+  let dir = 0;
+  if (rowA.columnFiltersMeta[columnId]) {
+    dir = compareItems(
+      rowA.columnFiltersMeta[columnId]?.itemRank!,
+      rowB.columnFiltersMeta[columnId]?.itemRank!
+    );
+  }
+
+  return dir === 0 ? sortingFns.alphanumeric(rowA, rowB, columnId) : dir;
+};
