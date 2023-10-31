bfb968dcaa854d038a4dd1b095b59380412bf9f5
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-10-31 17:22:26 +0700

    refactor: separate TextField as uncontrolled and controlled

--- app/lib/components/TextField.tsx
+++ app/lib/components/TextField.tsx
@@ -1,15 +1,7 @@
-import { cn } from '@lib/utils';
-import clsx from 'clsx';
-import { forwardRef, useEffect, useState } from 'react';
-import {
-  TextField as AriaTextField,
-  Text,
-  type TextFieldProps,
-} from 'react-aria-components';
-import { SwitchTransition } from 'transition-hook';
+import { forwardRef } from 'react';
+import { type TextFieldProps } from 'react-aria-components';
 import { useFormFieldsContext } from './Form';
-import Input from './Input';
-import Label from './Label';
+import UncontrolledTextField from './UncontrolledTextField';
 
 interface Props extends TextFieldProps {
   name: string;
@@ -21,69 +13,18 @@ interface Props extends TextFieldProps {
 }
 
 const TextField = forwardRef<HTMLInputElement, Props>(function TextField(
-  {
-    label,
-    labelClassName,
-    inputClassName,
-    description,
-    errorMessage,
-    ...props
-  }: Props,
+  { defaultValue, errorMessage, ...props }: Props,
   ref
 ) {
   const { [props.name]: field } = useFormFieldsContext() ?? {};
-  const [errorDisplay, setErrorDisplay] = useState(
-    errorMessage ?? field?.error
-  );
-  const invalid = !!errorMessage || !!field?.error;
-
-  useEffect(() => {
-    if (!errorMessage && !field?.error) {
-      return;
-    }
-    setErrorDisplay(errorMessage ?? field?.error);
-  }, [errorMessage, field?.error]);
 
   return (
-    <AriaTextField
-      defaultValue={field.defaultValue}
+    <UncontrolledTextField
       {...props}
-      isInvalid={!!props.isInvalid || invalid}
-    >
-      <Label className={cn('mb-0.5', labelClassName)}>{label}</Label>
-      <Input
-        ref={ref}
-        required={!!props.isRequired}
-        name={props.name}
-        className={cn(inputClassName, {
-          'peer border-negative-500': invalid,
-        })}
-      />
-      <SwitchTransition state={invalid} timeout={200} mode="out-in">
-        {(invalid, stage) => (
-          <div
-            className={clsx(
-              'transition-opacity duration-200 text-sm',
-              {
-                from: 'opacity-0 ease-in',
-                enter: '',
-                leave: 'opacity-0 ease-out',
-              }[stage]
-            )}
-          >
-            {invalid ? (
-              <Text slot="errorMessage" className="text-negative-500">
-                {errorDisplay + (errorDisplay?.at(-1) === '.' ? '' : '.')}
-              </Text>
-            ) : description ? (
-              <Text slot="description" className="text-primary-700">
-                {description + (description.at(-1) === '.' ? '' : '.')}
-              </Text>
-            ) : null}
-          </div>
-        )}
-      </SwitchTransition>
-    </AriaTextField>
+      ref={ref}
+      defaultValue={defaultValue ?? field.defaultValue}
+      errorMessage={errorMessage ?? field?.error}
+    />
   );
 });
 
