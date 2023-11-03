047a00636221813e8eeb2d3e438364f4a6c388ab
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-11-04 01:13:32 +0700

    style(Label): remove base style

--- app/lib/components/Label.tsx
+++ app/lib/components/Label.tsx
@@ -1,12 +1,5 @@
-import { cn } from '@lib/utils';
 import { Label as AriaLabel, type LabelProps } from 'react-aria-components';
 
-const baseClass = 'text-sm text-primary-700';
-
-export default function Label({ className, children, ...props }: LabelProps) {
-  return (
-    <AriaLabel {...props} className={cn(baseClass, className)}>
-      {children}
-    </AriaLabel>
-  );
+export default function Label(props: LabelProps) {
+  return <AriaLabel {...props} />;
 }
