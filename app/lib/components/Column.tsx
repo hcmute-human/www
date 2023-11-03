6576a710090771c7685bef40d6e998e620532575
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-11-03 22:37:44 +0700

    style(Column): brighter background

--- app/lib/components/Column.tsx
+++ app/lib/components/Column.tsx
@@ -10,7 +10,7 @@ export default function Column({ className, ...props }: Props) {
     <th
       {...props}
       className={cn(
-        'sticky top-0 px-4 py-2 border-b border-primary-200 bg-primary-100 font-bold text-left cursor-default first:rounded-tl last:rounded-tr whitespace-nowrap outline-none',
+        'sticky top-0 px-4 py-2 border-b border-primary-200 bg-primary-50 font-bold text-left cursor-default first:rounded-tl last:rounded-tr whitespace-nowrap outline-none',
         className
       )}
     />
