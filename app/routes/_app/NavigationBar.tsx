b908e5bf451bdb2b05b0e641feb8bcce9ca9af3f
duydang2311 <34796192+duydang2311@users.noreply.github.com>
2023-11-04 22:09:21 +0700

    refactor(NavigationBar): use root pathname

--- app/routes/_app/NavigationBar.tsx
+++ app/routes/_app/NavigationBar.tsx
@@ -1,3 +1,4 @@
+import { useLocation } from '@remix-run/react';
 import NavigationBarItem from './NavigationBarItem';
 import type { INavigationBarItem } from './types';
 
@@ -6,11 +7,18 @@ type Props = {
 };
 
 export default function NavigationBar({ items }: Props) {
+  const { pathname } = useLocation();
+  const rootPathName = pathname.split('/', 2).join('/');
+
   return (
     <nav>
       <ul className="flex gap-1 lg:block lg:space-y-1">
         {items.map((item) => (
-          <NavigationBarItem key={item.href} item={item} />
+          <NavigationBarItem
+            key={item.href}
+            item={item}
+            isActive={rootPathName === item.href}
+          />
         ))}
       </ul>
     </nav>
